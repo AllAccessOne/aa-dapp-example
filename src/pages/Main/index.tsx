9746e59caa0c36846886d2e4e7300687dd48d6ab
Kiet1618 <ngokiet3207@gmail.com>
2023-06-16 04:47:20 +0700

    update sign and send transaction at wallet

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -25,126 +25,6 @@ const steps = ['Start', 'Pending', 'Success'];
 
 const Main = () => {
     const myAddress = "0x04E407C7d7C2A6aA7f2e66B0B8C0dBcafA5E3Afe";
-    const { web3 } = useBlockchain("https://goerli.blockpi.network/v1/rpc/public");
-    const [openAlert, setOpenAlert] = React.useState(false);
-    const handleCloseAlert = (event?: React.SyntheticEvent | Event, reason?: string) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpenAlert(false);
-    };
-    const [typeTransaction, setTypeTransaction] = useState<"success" | "error">("success");
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const [infoTransaction, setInfoTransaction] = useState("");
-    const [transactionHash, setTransactionHash] = useState("");
-    const sendTransaction = async (web3: Web3, data: FormData) => {
-        const privateKey = "e19d5570950218a0f385f098b9b6c050a63aca31a286da271075a33a8ae7b3da";
-        // const privateKey = "";
-        const { addressTo, amount } = data;
-        try {
-            const weiValue = Math.round(parseFloat(amount) * 10 ** 18);
-            const hexValue = web3.utils.toHex(weiValue ? weiValue : 0);
-
-            const price = await web3.eth.getGasPrice();
-            const gasLimit = await web3.eth.estimateGas({
-                to: addressTo,
-                from: web3.defaultAccount as string,
-                value: hexValue,
-                data: "0x",
-            });
-
-            const tx = {
-                to: addressTo,
-                from: web3.defaultAccount as string,
-                value: hexValue,
-                gas: gasLimit,
-                gasPrice: price,
-                data: "0x",
-            };
-            const signedTransaction: any = await web3.eth.accounts.signTransaction(tx, privateKey);
-            const sendSignedTransaction = web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
-            sendSignedTransaction
-                .on("transactionHash", (transactionHash) => {
-                    setTransactionHash(transactionHash);
-                    setInfoTransaction("start");
-                })
-            await sendSignedTransaction;
-            setInfoTransaction("success");
-            setTransactionHash("");
-            setTypeTransaction("success")
-        } catch (error) {
-            setInfoTransaction("error");
-            handleClose();
-            handleAlert();
-            setTypeTransaction("error")
-
-        }
-    };
-    const handleAlert = () => {
-        setOpenAlert(true);
-    };
-    useEffect(() => {
-        if (infoTransaction === "success" || infoTransaction === "start") {
-            handleComplete();
-            handleNext()
-            if (infoTransaction === "success") {
-                handleClose();
-                handleAlert();
-            }
-        }
-        setInfoTransaction("");
-        console.log(infoTransaction);
-    }, [infoTransaction])
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [completed, setCompleted] = React.useState<{
-        [k: number]: boolean;
-    }>({});
-
-    const totalSteps = () => {
-        return steps.length;
-    };
-
-    const completedSteps = () => {
-        return Object.keys(completed).length;
-    };
-
-    const isLastStep = () => {
-        return activeStep === totalSteps() - 1;
-    };
-
-    const allStepsCompleted = () => {
-        return completedSteps() === totalSteps();
-    };
-
-    const handleNext = () => {
-        const newActiveStep =
-            isLastStep() && !allStepsCompleted()
-                ? // It's the last step, but not all steps have been completed,
-                // find the first step that has been completed
-                steps.findIndex((step, i) => !(i in completed))
-                : activeStep + 1;
-        setActiveStep(newActiveStep);
-    };
-
-
-    const handleStep = (step: number) => () => {
-        setActiveStep(step);
-    };
-
-    const handleComplete = () => {
-        const newCompleted = completed;
-        newCompleted[activeStep] = true;
-        setCompleted(newCompleted);
-        handleNext();
-    };
-
-    const handleReset = () => {
-        setActiveStep(0);
-        setCompleted({});
-    };
 
     return (
         <>
@@ -189,17 +69,17 @@ const Main = () => {
                         </Typography>
                         <Button
                             size="large"
+                            href="http://localhost:3000/sign-transaction/%7B%22to%22:%20%220x1234567890abcdef%22,%22value%22:%20%220.001%22%7D"
+                            target="_blank"
                             style={{ marginTop: "20px", borderRadius: '10px' }}
                             fullWidth
                             variant="contained"
                             color="primary"
                             onClick={() => {
-                                sendTransaction(web3 as Web3, {
-                                    addressTo: myAddress,
-                                    amount: "0.00001"
-                                })
-                                handleOpen();
-                            }}
+
+                            }
+
+                            }
                         > Send Transaction</Button>
                     </CardContent>
                 </Card>
@@ -219,72 +99,19 @@ const Main = () => {
                         </Typography>
                         <Button
                             size="large"
+                            href="http://localhost:3000/sign-transaction/%7B%22to%22:%20%220x04e407c7d7c2a6aa7f2e66b0b8c0dbcafa5e3afe%22,%22value%22:%20%220.001%22,%20%22tokenContract%22%20:%20%220x73967c6a0904aA032C103b4104747E88c566B1A2%22%7D"
                             style={{ marginTop: "20px", borderRadius: '10px' }}
                             fullWidth
+                            target="_blank"
                             variant="contained"
                             color="primary"
+                            onClick={() => {
+                            }
+                            }
                         > Send Transaction</Button>
                     </CardContent>
                 </Card>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Stepper nonLinear activeStep={activeStep}>
-                            {steps.map((label, index) => (
-                                <Step key={label} completed={completed[index]}>
-                                    <StepButton color="inherit" onClick={handleStep(index)}>
-                                        {label}
-                                    </StepButton>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        <div>
-                            {allStepsCompleted() ? (
-                                <React.Fragment>
-                                    <Typography sx={{ mt: 2, mb: 1 }}>
-                                        All steps completed - you&apos;re finished
-                                    </Typography>
-                                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                                        <Box sx={{ flex: '1 1 auto' }} />
-                                        <Button onClick={handleReset}>Reset</Button>
-                                    </Box>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
 
-                                        {
-                                            transactionHash ?
-                                                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                                    <CircularProgress style={{ marginBottom: '20px' }} />
-                                                </Box>
-                                                : null
-                                        }
-                                        {
-                                            transactionHash ?
-                                                "Transaction Hash: " + transactionHash
-
-                                                :
-                                                null
-                                        }
-
-                                    </Typography>
-
-                                </React.Fragment>
-                            )}
-                        </div>
-                    </Box>
-                </Modal>
-                <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
-                    <Alert onClose={handleCloseAlert} severity={typeTransaction} sx={{ width: '100%' }}>
-                        {typeTransaction == "success" ? "Successfully Transaction!" : "Failed Transaction!"}
-
-                    </Alert>
-                </Snackbar>
             </BodyApp>
         </>
     );
